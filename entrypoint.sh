71ba2d5cca5ccbb31e9b1c333e85d4337b44c36d
Matthew Kobayashi <1856537+MattKobayashi@users.noreply.github.com>
2025-09-15 17:20:30 +1000

    feat: add support for multiple domains (#25)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
+PRIMARY_DOMAIN=${DOMAIN%%,*}
+PRIMARY_DOMAIN=$(printf "%s" "$PRIMARY_DOMAIN" | sed -e 's/^ *//' -e 's/ *$//')
+
 cat > cli.ini << EOF
-dns-cloudflare = True
 agree-tos = True
-no-eff-email = True
-keep-until-expiring = True
-dns-cloudflare-propagation-seconds = 30
+dns-cloudflare = True
 dns-cloudflare-credentials = /run/secrets/CERTBOT_CF_DNS_API_TOKEN
-deploy-hook = cp -RL /etc/letsencrypt/live/$DOMAIN/ /opt/certs/ && chmod -R o+r /opt/certs/
+dns-cloudflare-propagation-seconds = 30
 domain = $DOMAIN
 email = $EMAIL
+expand = True
+keep-until-expiring = True
+no-eff-email = True
+post-hook = openssl pkcs12 -export -out /etc/letsencrypt/live/$PRIMARY_DOMAIN/cert_key.p12 -inkey /etc/letsencrypt/live/$PRIMARY_DOMAIN/privkey.pem -in /etc/letsencrypt/live/$PRIMARY_DOMAIN/cert.pem -certfile /etc/letsencrypt/live/$PRIMARY_DOMAIN/chain.pem -passout pass:
 EOF
 
+certbot certonly --config /opt/certbot/cli.ini
 exec /usr/bin/supercronic /crontab/certbot-cron
