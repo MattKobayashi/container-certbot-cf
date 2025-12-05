ab6012bbe25267927254000d5afe476dbaaf1cd3
Matthew Kobayashi <matthew@kobayashi.au>
2025-12-06 01:20:00 +1000

    fix: shellcheck

--- renewal-hooks/deploy/01-pihole.sh
+++ renewal-hooks/deploy/01-pihole.sh
@@ -3,5 +3,5 @@
 PRIMARY_DOMAIN=${DOMAIN%%,*}
 PRIMARY_DOMAIN=$(printf "%s" "$PRIMARY_DOMAIN" | sed -e 's/^ *//' -e 's/ *$//' -e 's/^\*\.//')
 
-cp /etc/letsencrypt/live/$PRIMARY_DOMAIN/cert.pem /opt/certs/tls.pem
-cat /etc/letsencrypt/live/$PRIMARY_DOMAIN/privkey.pem /opt/certs/tls.pem
+cp /etc/letsencrypt/live/"$PRIMARY_DOMAIN"/cert.pem /opt/certs/tls.pem
+cat /etc/letsencrypt/live/"$PRIMARY_DOMAIN"/privkey.pem /opt/certs/tls.pem
