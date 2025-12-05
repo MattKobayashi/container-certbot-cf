fe28abf0de7991cfc6ea3222736f327ae7b49258
Matthew Kobayashi <1856537+MattKobayashi@users.noreply.github.com>
2025-12-06 01:25:40 +1000

    feat: produce tls.pem for Pi-Hole (#43)
    * feat: move hooks to separate scripts
    
    * fix: shellcheck
    
    * fix: various

--- tests/entrypoint_test.sh
+++ tests/entrypoint_test.sh
@@ -4,16 +4,16 @@ PRIMARY_DOMAIN=${DOMAIN%%,*}
 PRIMARY_DOMAIN=$(printf "%s" "$PRIMARY_DOMAIN" | sed -e 's/^ *//' -e 's/ *$//' -e 's/^\*\.//')
 
 cat > cli.ini << EOF
-# dns-cloudflare = True
-standalone = True
 agree-tos = True
-no-eff-email = True
-keep-until-expiring = True
-# dns-cloudflare-propagation-seconds = 30
+# dns-cloudflare = True
 # dns-cloudflare-credentials = /run/secrets/CERTBOT_CF_DNS_API_TOKEN
+# dns-cloudflare-propagation-seconds = 30
 deploy-hook = cp -RL /etc/letsencrypt/live/$PRIMARY_DOMAIN/ /opt/certs/ && chmod -R o+r /opt/certs/
 domain = $DOMAIN
 email = $EMAIL
+keep-until-expiring = True
+no-eff-email = True
+standalone = True
 EOF
 
 exec certbot certonly --config cli.ini --dry-run --no-verify-ssl --non-interactive --server https://10.30.50.2:14000/dir --verbose
